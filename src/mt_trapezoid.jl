48015276042c125a1d0d52533bfef7c1186f5856
Simone <smarras@njit.edu>
2023-01-30 20:30:44 -0500

    symbol for integral added to mt_trapezoid.jl

--- src/mt_trapezoid.jl
+++ src/mt_trapezoid.jl
@@ -1,9 +1,10 @@
-function quad_trapezoid(f; a, b, Nq)   
+function quad_trapezoid(f; a, b, Nq)
+    
     Δx = (b - a)/Nq
-    If = Δx * (f(a) + f(b))/2
+    ∫ = Δx*(f(a) + f(b))/2
     for k = 1:Nq - 1
-        xk = (b - a) * k/Nq + a
-        If = If + Δx*f(xk)
+        xk = (b - a)*k/Nq + a
+        ∫ = ∫ + Δx*f(xk)
     end
-    return If
+    return ∫
 end
