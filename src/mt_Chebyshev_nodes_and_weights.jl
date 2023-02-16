9a696ea9e8be192bb7dee2abe9d794a985c9026c
Simone <smarras@njit.edu>
2023-02-16 08:49:49 -0500

    Added mt_Chebyshev_nodes_and_weights.jl

---
+++ src/mt_Chebyshev_nodes_and_weights.jl
@@ -0,0 +1,17 @@
+using("mt_Chebyshev.jl")
+
+function ChebyshevGaussNodesAndWeights!(cg::St_lg, nop::TInt)
+       for j=0:nop
+        cg.ξ[j+1]=-cospi((2*j+1)/(2*nop+2))
+        cg.ω[j+1]=π/(nop+1)
+    end
+end
+
+function ChebyshevGaussLobattoNodesAndWeights!(cgl::St_cgl,nop::TInt)
+    for j=0:nop
+        cgl.ξ[j+1]=-cospi(j/nop)
+        cgl.ω[j+1]=π/nop
+    end
+    cgl.ω[1]=cgl.ω[1]/2
+    cgl.ω[nop+1]=cgl.ω[nop+1]/2
+end
