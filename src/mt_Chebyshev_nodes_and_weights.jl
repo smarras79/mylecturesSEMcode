590d76480f59b6107b1900cd81bf206219e64fe1
Simone Marras <46322845+smarras79@users.noreply.github.com>
2023-03-06 13:49:42 -0500

    Update mt_Chebyshev_nodes_and_weights.jl

--- src/mt_Chebyshev_nodes_and_weights.jl
+++ src/mt_Chebyshev_nodes_and_weights.jl
@@ -1,13 +1,13 @@
 using("mt_Chebyshev.jl")
 
-function ChebyshevGaussNodesAndWeights!(cg::St_lg, nop::TInt)
+function ChebyshevGaussNodesAndWeights!(cg::St_lg, nop::Int64)
        for j=0:nop
         cg.ξ[j+1]=-cospi((2*j+1)/(2*nop+2))
         cg.ω[j+1]=π/(nop+1)
     end
 end
 
-function ChebyshevGaussLobattoNodesAndWeights!(cgl::St_cgl,nop::TInt)
+function ChebyshevGaussLobattoNodesAndWeights!(cgl::St_cgl,nop::Int64)
     for j=0:nop
         cgl.ξ[j+1]=-cospi(j/nop)
         cgl.ω[j+1]=π/nop
